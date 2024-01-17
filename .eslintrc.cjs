a85fef9de57138aa05b1882a7ee4a2e4320d2097
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-01-17 07:40:46 -0700

    build(eslint): mark `prettier/prettier` as eslint warnings
      ## what
      - mark `prettier/prettier` as eslint warnings
    
      ## how
    
      ## why
    
      ## where
      - ./.eslintrc.cjs
    
      ## usage

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,10 @@ const config = {
     "next/core-web-vitals",
     "prettier",                       // npm package eslint-config-prettier
     "plugin:prettier/recommended",    // npm package eslint-plugin-prettier
-  ]
+  ],
+  rules: {
+    "prettier/prettier": "warn",
+  }
 }
 
-module.exports = config
+module.exports = config;
