78417e2e68122a319100e901d771369d38d02c9a
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-01-17 07:54:02 -0700

    build(eslint): add eslint typescript
      ## what
      - add eslint typescript
        - extend rules
        - add plugin
        - add parser
    
      ## how
    
      ## why
    
      ## where
      - ./.eslintrc.cjs
    
      ## usage

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,18 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
+  root: true,
   "extends": [
     "next",
     "next/core-web-vitals",
     "prettier",                       // npm package eslint-config-prettier
     "plugin:prettier/recommended",    // npm package eslint-plugin-prettier
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"   // npm package @typescript-eslint/eslint-plugin
   ],
+  plugins: [
+    '@typescript-eslint'
+  ],
+  parser: '@typescript-eslint/parser',
   rules: {
     "prettier/prettier": "warn",
   }
