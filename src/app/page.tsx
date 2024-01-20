ea8ecdf435e75dca7e199c87594c70288c509878
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-01-20 16:27:25 -0700

    chore(page:home): display ENVs
      ## what
      - display ENVs
    
      ## how
    
      ## why
      - to test if ENV values are being displayed properly
    
      ## where
      - ./src/app/page.tsx
    
      ## usage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,26 @@ import Image from 'next/image';
 const Home = () => {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <div>
+        <table>
+          <tr>
+            <td className='pr-2'><h1>NEXT_PUBLIC_BUILD_VERSION</h1></td>
+            <td className='pl-2 font-bold'>{process.env.NEXT_PUBLIC_BUILD_VERSION}</td>
+          </tr>
+          <tr>
+            <td className='pr-2'><h1>NEXT_PUBLIC_BUILD_ID</h1></td>
+            <td className='pl-2 font-bold'>{process.env.NEXT_PUBLIC_BUILD_ID}</td>
+          </tr>
+          <tr>
+            <td className='pr-2'><h1>NEXT_PUBLIC_BUILD_TIME</h1></td>
+            <td className='pl-2 font-bold'>{process.env.NEXT_PUBLIC_BUILD_TIME}</td>
+          </tr>
+          <tr>
+            <td className='pr-2'><h1>NEXT_PUBLIC_TEST</h1></td>
+            <td className='pl-2 font-bold'>{process.env.NEXT_PUBLIC_TEST}</td>
+          </tr>
+        </table>
+      </div>
       <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
         <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
           Get started by editing&nbsp;
