ffda14dc50a3add07fb5d1f0d0c8e017cab1a123
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-01-23 06:44:57 -0700

    feat(page:home): add darkmode toggle
      ## what
      - add darkmode toggle
    
      ## how
    
      ## why
      - to be able to toggle between darkmode and lightmode
    
      ## where
      - ./src/app/page.tsx
    
      ## usage
    
      ## issue or pull request
      - #15

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 
+import { ModeToggle } from '@/components/darkmode-toggle';
+
 const Home = () => {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
@@ -22,6 +24,9 @@ const Home = () => {
             <td className='pl-2 font-bold'>{process.env.NEXT_PUBLIC_TEST}</td>
           </tr>
         </table>
+      </div>
+      <div>
+        <ModeToggle />
       </div>
       <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
         <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
