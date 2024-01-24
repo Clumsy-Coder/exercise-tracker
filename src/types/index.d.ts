b77f31940eecd1c4cb2ee9899c277003f63ea069
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-01-24 08:15:09 -0700

    chore(types): add type for site config
      ## what
      - add type for site config
    
      ## how
      - obtained from
        - https://github.com/shadcn-ui/taxonomy/blob/651f984e52edd65d40ccd55e299c1baeea3ff017/types/index.d.ts#L30-L39
    
      ## why
      - used for defining site config
        - helpful for SEO
    
      ## where
      - ./src/types/index.d.ts
    
      ## usage
    
      ## issue or pull request

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -13,3 +13,29 @@ export type NavbarLink = {
    */
   href: string;
 };
+
+// ---------------------------------------------------------------------------------------------- //
+
+/**
+ * Site configuration
+ *
+ * obtained from
+ * - https://github.com/shadcn-ui/taxonomy/blob/651f984e52edd65d40ccd55e299c1baeea3ff017/types/index.d.ts#L30-L39
+ */
+export type SiteConfig = {
+  /**
+   * Name of the site
+   */
+  name: string;
+  /**
+   * Site description.
+   * for SEO
+   */
+  description: string;
+  // url: string;
+  // ogImage: string;
+  // links: {
+  //   twitter: string;
+  //   github: string;
+  // };
+};
