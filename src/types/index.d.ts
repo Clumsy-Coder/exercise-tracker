4b00b402785410cd36f3379f7a359a2679784061
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-01-23 18:07:44 -0700

    chore(types): add type for nav link
      ## what
      - add type for nav link
    
      ## how
    
      ## why
      - this will be used for defining an array of nav links
    
      ## where
      - ./src/types/index.d.ts
    
      ## usage
    
      ## issue or pull request

---
+++ src/types/index.d.ts
@@ -0,0 +1,15 @@
+/**
+ * Navbar link item.
+ *
+ * Used in Navbar
+ */
+export type NavbarLink = {
+  /**
+   * Link to display
+   */
+  title: string;
+  /**
+   * URL of the link
+   */
+  href: string;
+};
