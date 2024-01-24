1eabb0638c5533943ac565db58018f81cb68b007
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-01-24 08:31:52 -0700

    chore(layout): render navbar component
      ## what
      - render navbar component
    
      ## how
      - obtained from
        - https://github.com/shadcn-ui/taxonomy/blob/main/app/(dashboard)/dashboard/layout.tsx
    
      ## why
    
      ## where
      - ./src/app/layout.tsx
    
      ## usage
    
      ## issue or pull request
      #17

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { ThemeProvider } from '@/components/theme-provider';
+import Navbar from '@/components/navbar';
+import { ModeToggle } from '@/components/darkmode-toggle';
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -26,7 +28,24 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          {/*
+              obtained from
+              https://github.com/shadcn-ui/taxonomy/blob/main/app/(dashboard)/dashboard/layout.tsx
+          */}
+          <div className='flex min-h-screen flex-col space-y-6'>
+            <header className='sticky top-0 z-40 border-b bg-background'>
+              <div className='container flex h-16 items-center justify-between py-4'>
+                <Navbar />
+                <ModeToggle />
+              </div>
+            </header>
+            {/* <div className='container grid flex-1 gap-12 md:grid-cols-[200px_1fr]'> */}
+            {/*   <aside className='hidden w-[200px] flex-col md:flex'> */}
+            {/*     <DashboardNav items={dashboardConfig.sidebarNav} /> */}
+            {/*   </aside> */}
+            <main className='flex w-full flex-1 flex-col overflow-hidden'>{children}</main>
+            {/* </div> */}
+          </div>
           <Analytics />
         </ThemeProvider>
       </body>
