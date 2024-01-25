42ebd2749e75db2519d13750265c5fa72bc81910
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-01-24 18:28:29 -0700

    feat(components:navbar): add Drawer for smaller screens
      ## what
      - add Drawer for smaller screens
        - add button to open drawer
        - display `menu` button on smaller screens
        - add nav links in drawer
        - navigate to different page when pressing on link
        - close drawer when pressing on nav link
        - add close menu button in drawer
    
      ## how
      - using shadcn-ui `Drawer` component
        - https://ui.shadcn.com/docs/components/drawer
      - use the `useState` to open or close the drawer
        - this is for closing the drawer after pressing on the link in the
          drawer
    
      ## why
      - This will be used on smaller screens to display nav links
    
      ## where
      - ./src/components/navbar.tsx
    
      ## usage
    
      ## issue or pull request
      #19

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,21 +1,76 @@
 'use client';
 
-import { Dumbbell } from 'lucide-react';
+import { Dumbbell, Menu } from 'lucide-react';
 import Link from 'next/link';
 import { useSelectedLayoutSegment } from 'next/navigation';
 
+import { Button, buttonVariants } from '@/components/ui/button';
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+} from '@/components/ui/drawer';
 import { siteConfig } from '@/site/config';
 import { cn } from '@/lib/utils';
 import { links } from '@/utils';
+import { useState } from 'react';
 
 // obtained from
 // https://github.com/shadcn-ui/taxonomy/blob/main/components/main-nav.tsx
 const Navbar = () => {
+  // used for opening and closing drawer
+  // needed because the drawer needs to close after pressing the link in the drawer
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
   const segment = useSelectedLayoutSegment();
 
   return (
     <div>
-      <nav className='flex items-center gap-6 md:gap-10'>
+      <nav className='flex items-center gap-4 md:gap-10'>
+        {/* Drawer for mobile screens */}
+        <aside className='sm:hidden'>
+          <Button
+            size='icon'
+            variant='ghost'
+            onClick={() => setDrawerIsOpen(true)}
+          >
+            <Menu />
+          </Button>
+          <Drawer
+            open={drawerIsOpen}
+            onOpenChange={setDrawerIsOpen}
+          >
+            <DrawerContent>
+              <DrawerHeader>
+                <DrawerDescription className='flex flex-col items-center justify-center gap-2'>
+                  {links.slice(1).map((item) => (
+                    <Link
+                      key={`navlink-${item.title}`}
+                      href={item.href}
+                      className={cn(buttonVariants({ size: 'lg' }), 'h-16 text-xl', 'w-full')}
+                      onClick={() => setDrawerIsOpen(false)}
+                    >
+                      {item.title}
+                    </Link>
+                  ))}
+                </DrawerDescription>
+              </DrawerHeader>
+              <DrawerFooter>
+                <DrawerClose>
+                  <Button
+                    variant='outline'
+                    size='lg'
+                  >
+                    Close menu
+                  </Button>
+                </DrawerClose>
+              </DrawerFooter>
+            </DrawerContent>
+          </Drawer>
+        </aside>
+        {/*-------------------------------------------------------------------------------------*/}
         {/* App link  */}
         <Link
           href='/'
@@ -24,6 +79,7 @@ const Navbar = () => {
           <Dumbbell className='h-[1.0rem] w-[1.0rem]' />
           <span className='font-bold sm:inline-block'>{siteConfig.name}</span>
         </Link>
+        {/*-------------------------------------------------------------------------------------*/}
         {/* Other links */}
         {links.slice(1).map((item) => (
           <Link
