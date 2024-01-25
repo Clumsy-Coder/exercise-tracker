49dd3bfe66f3fb16b40fb4d3dd4117229a5a931e
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-01-25 16:57:20 -0700

    build(api): add headers for fetching ExerciseDB
      ## what
      - add headers for fetching ExerciseDB
    
      ## how
    
      ## why
      - used for fetching data from ExerciseDB
    
      ## where
      - ./src/utils/fetchData.ts
    
      ## usage
    
      ## issue or pull request

---
+++ src/utils/fetchData.ts
@@ -0,0 +1,10 @@
+/**
+ * Fetch header for Exercise DB
+ */
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.EXERCISE_DB_API,
+  },
+};
