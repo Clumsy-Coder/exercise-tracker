a1bafa0ba0530044708d24ad0bdd89c2172ffe5a
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-02-10 12:01:40 -0700

    refactor(api:exercises): set limit when fetching all exercises
      ## what
      - set limit when fetching all exercises
    
      ## how
    
      ## why
      - by default the upstream api will return 10 exercises
      - this endpoint will be used for fetching all exercises
    
      ## where
      - ./src/app/api/exercises/route.ts
    
      ## usage
    
      ## issue or pull request
      - commit dd5c5c8

--- src/app/api/exercises/route.ts
+++ src/app/api/exercises/route.ts
@@ -5,7 +5,7 @@ import { exerciseOptions, fetchAllExercises } from '@/utils/fetchData';
  * Fetch all exercises
  */
 export const GET = async () => {
-  const url = fetchAllExercises();
+  const url = fetchAllExercises(20000);
   const response = await fetch(url, exerciseOptions);
   const data: Exercise[] = await response.json();
 
