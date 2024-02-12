63a7571922e16016e5efd601def271a3e4588bf0
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-02-12 10:33:40 -0700

    feat(components): add component `ExerciseCard`
      ## what
      - add component `ExerciseCard`
        - exercise name
        - equipment
        - exercise gif
        - exercise target
        - secondary muscles
        - link to the exercise when hovering on the card
    
      ## how
    
      ## why
      - this will be used to display a single Exercise
    
      ## where
      - ./src/components/exerciseCard.tsx
    
      ## usage
    
      ## issue or pull request

---
+++ src/components/exerciseCard.tsx
@@ -0,0 +1,94 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { Badge, badgeVariants } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { cn } from '@/lib/utils';
+import { Exercise } from '@/types/raw';
+
+type Props = {
+  data: Exercise;
+};
+
+/**
+ * Component for displaying a single Exercise
+ *
+ * Display
+ * - Exercise name
+ * - Exercise equipment
+ * - Exercise GIF
+ * - Exercise target
+ * - Secondary muscles
+ */
+const ExerciseCard = ({ data }: Props) => {
+  const { id, name, gifUrl, target, equipment } = data;
+
+  return (
+    <Card className='transition ease-in-out hover:ring hover:ring-neutral-300'>
+      <CardHeader>
+        <div className='flex items-center justify-between'>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <CardTitle className='truncate py-2 pb-0.5 capitalize'>{name}</CardTitle>
+              </TooltipTrigger>
+              <TooltipContent className='capitalize'>{name}</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          {/* Link to exercise */}
+          <Button asChild>
+            <Link href={`/exercises/${id}`}>View more</Link>
+          </Button>
+        </div>
+        <CardDescription className='py-1 capitalize '>
+          <Link
+            href={`/exercises/equipment/${equipment}`}
+            className='hover:underline hover:underline-offset-4'
+            // className='underline underline-offset-4 hover:text-primary'
+          >
+            {equipment}
+          </Link>
+        </CardDescription>
+      </CardHeader>
+      <CardContent className='relative my-3 h-72 w-full'>
+        <Image
+          src={gifUrl}
+          alt={`${name}-gif`}
+          fill
+          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+          loading='lazy'
+          className='object-contain'
+        />
+      </CardContent>
+      <CardFooter className='flex gap-1.5 overflow-auto pt-1'>
+        {/* <Badge className='capitalize'>{target}</Badge> */}
+        <Link
+          href={`/exercises/target/${target}`}
+          className={cn(badgeVariants(), 'capitalize', 'whitespace-nowrap')}
+        >
+          {target}
+        </Link>
+        {data.secondaryMuscles.map((item) => (
+          <Badge
+            key={`${id}-secondary-${item}`}
+            variant='secondary'
+            className='whitespace-nowrap capitalize'
+          >
+            {item}
+          </Badge>
+        ))}
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default ExerciseCard;
