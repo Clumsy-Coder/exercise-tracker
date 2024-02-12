5028a6907ce3b43fc567c1519039600b97334283
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-02-12 10:28:51 -0700

    build(tailwind): add some custom colours
      ## what
      - add some custom colours
    
      ## how
    
      ## why
    
      ## where
      - ./tailwind.config.ts
    
      ## usage
    
      ## issue or pull request

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -52,6 +52,18 @@ const config: Config = {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
         },
+        // ------------------------------------------------------------------------------------- //
+        'black-100': '#2B2C35',
+        'primary-blue': {
+          DEFAULT: '#2B59FF',
+          100: '#F5F8FF',
+        },
+        'secondary-orange': '#f79761',
+        'light-white': {
+          DEFAULT: 'rgba(59,60,152,0.03)',
+          100: 'rgba(59,60,152,0.02)',
+        },
+        grey: '#747A88',
       },
       borderRadius: {
         lg: 'var(--radius)',
