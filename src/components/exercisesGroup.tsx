9e8ab68e379e0fb7e55deb006e297b6cc3832b96
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-02-12 16:13:08 -0700

    feat(components:exercisesGroup): add basic pagination
      ## what
      - add basic pagination
    
      ## how
      - use `useState` to keep track of number of paginations displayed
      - slice the exercises array from the start up to the pagination value
        from `useState`
        - display exercise cards using the sliced array
      - every time `show more` button is pressed, increase number of cards
        displayed by setting the `useState` value
      - set the pagination value to the number of elements in exercises
        array when the `show all` is pressed
      - disable `show more` or `show all` button when the number of
        exercises cards displayed is set to max
    
      ## why
      - the `/exercises` page will load all the exercises. This can be
        difficult to render all the exercise cards
      - add pagination will reduce memory usage when rendering
    
      ## where
      - ./src/components/exercisesGroup.tsx
    
      ## usage
    
      ## issue or pull request

--- src/components/exercisesGroup.tsx
+++ src/components/exercisesGroup.tsx
@@ -1,23 +1,44 @@
 'use client';
 
-import { Exercise } from '@/types/raw';
+import { useState } from 'react';
+
 import ExerciseCard from '@/components/exerciseCard';
+import { Button } from '@/components/ui/button';
+import { Exercise } from '@/types/raw';
 
 type Props = {
   exercises: Exercise[];
 };
 
 const ExercisesGroup = ({ exercises }: Props) => {
+  // number of exercises to display at a time
+  const [showLimit, setShowLimit] = useState(6);
+
   return (
     <div>
       <div className='exercises__exercises-wrapper'>
-        {exercises.map((item) => (
+        {exercises.slice(0, showLimit).map((item) => (
           // <p key={`${item.name}-${item.id}`}>{item.id} - {item.name}</p>
           <ExerciseCard
             key={`${item.name}-${item.id}`}
             data={item}
           />
         ))}
+      </div>
+      {/* Pagination */}
+      <div className='mb-8 mt-8 flex justify-center gap-8'>
+        <Button
+          onClick={() => setShowLimit(showLimit + 6)}
+          disabled={showLimit >= exercises.length}
+        >
+          Show more
+        </Button>
+        <Button
+          onClick={() => setShowLimit(exercises.length)}
+          disabled={showLimit === exercises.length}
+        >
+          Show all
+        </Button>
       </div>
     </div>
   );
