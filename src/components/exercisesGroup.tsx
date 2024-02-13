04e58e1635c0ff4c13a889303ba23c7b69d9852f
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-02-12 17:48:40 -0700

    feat(components:exerciseGroup): add ability to search exercises
      ## what
      - add ability to search exercises
        - search by exercise name
        - search by target
        - search by equipment
        - search by secondary muscles
      - display number exercises available to view (filtered or not)
    
      ## how
      - store the search string using `useState`
      - set the string when input changes
      - when the searchStr is not empty, filter the exercises
        - filter by exercise name
        - filter by target
        - filter by equipment
        - filter by secondary muscles
    
      ## why
      - to be able to search an exercise
    
      ## where
      - ./src/components/exercisesGroup.tsx
    
      ## usage
    
      ## issue or pull request

--- src/components/exercisesGroup.tsx
+++ src/components/exercisesGroup.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import ExerciseCard from '@/components/exerciseCard';
 import { Button } from '@/components/ui/button';
 import { Exercise } from '@/types/raw';
+import { Input } from '@/components/ui/input';
 
 type Props = {
   exercises: Exercise[];
@@ -16,11 +17,45 @@ const SHOW_LIMIT_INCREMENT = 6;
 const ExercisesGroup = ({ exercises }: Props) => {
   // number of exercises to display at a time
   const [showLimit, setShowLimit] = useState(DEFAULT_SHOW_LIMIT);
+  const [searchStr, setSearchString] = useState('');
+
+  let exercisesToDisplay: Exercise[];
+
+  if (searchStr) {
+    exercisesToDisplay = exercises.filter((item) => {
+      const searchStrLower = searchStr.toLowerCase();
+      return (
+        item.name.toLowerCase().replaceAll('-', ' ').includes(searchStrLower) ||
+        item.target.toLowerCase().includes(searchStrLower) ||
+        item.equipment.toLowerCase().includes(searchStrLower) ||
+        item.secondaryMuscles.some((item) => item.toLowerCase().includes(searchStrLower))
+      );
+    });
+  } else {
+    exercisesToDisplay = exercises;
+  }
 
   return (
     <div className='flex flex-col items-center'>
+      <div className='flex w-full items-center justify-center gap-5'>
+        {/* Search exercises */}
+        <Input
+          type='search'
+          placeholder='Search an exercise'
+          onChange={(event) => {
+            setSearchString(event.target.value);
+            setShowLimit(DEFAULT_SHOW_LIMIT);
+          }}
+          value={searchStr}
+          className='md:w-[40rem] '
+        />
+        {/* Display number of exercises displayed */}
+        <p className='whitespace-nowrap text-neutral-500 dark:text-neutral-400'>
+          {new Intl.NumberFormat('us').format(exercisesToDisplay.length).toString()} exercises
+        </p>
+      </div>
       <div className='exercises__exercises-wrapper'>
-        {exercises.slice(0, showLimit).map((item) => (
+        {exercisesToDisplay.slice(0, showLimit).map((item) => (
           // <p key={`${item.name}-${item.id}`}>{item.id} - {item.name}</p>
           <ExerciseCard
             key={`${item.name}-${item.id}`}
@@ -32,13 +67,13 @@ const ExercisesGroup = ({ exercises }: Props) => {
       <div className='mb-8 mt-8 flex justify-center gap-8'>
         <Button
           onClick={() => setShowLimit(showLimit + SHOW_LIMIT_INCREMENT)}
-          disabled={showLimit >= exercises.length}
+          disabled={showLimit >= exercisesToDisplay.length}
         >
           Show more
         </Button>
         <Button
-          onClick={() => setShowLimit(exercises.length)}
-          disabled={showLimit >= exercises.length}
+          onClick={() => setShowLimit(exercisesToDisplay.length)}
+          disabled={showLimit >= exercisesToDisplay.length}
         >
           Show all
         </Button>
