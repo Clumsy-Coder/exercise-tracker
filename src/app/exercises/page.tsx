c8cebb398a70411feba2ba5a379c1a98c195f06e
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-02-13 15:55:49 -0700

    feat(page:exercises): display exercise equipment list
      ## what
      - display exercise equipment list
        - fetch exercise equipment list
        - render equipment list as a link
    
      ## how
      - use react-query hook to fetch the data
    
      ## why
      - this will be used to display different exercise equipments
        - any of those can be clicked to view exercises of that equipment
    
      ## where
      - ./src/app/exercises/page.tsx
    
      ## usage
    
      ## issue or pull request
      - #22

--- src/app/exercises/page.tsx
+++ src/app/exercises/page.tsx
@@ -5,14 +5,22 @@ import Link from 'next/link';
 import ExercisesGroup from '@/components/exercisesGroup';
 import { buttonVariants } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
-import { useFetchAllExercises, useFetchTargetList } from '@/hooks';
+import { useFetchAllExercises, useFetchEquipmentList, useFetchTargetList } from '@/hooks';
 import { cn } from '@/lib/utils';
 
 const ExercisesPage = () => {
   const { isLoading, data } = useFetchAllExercises();
   const { isLoading: targetListIsLoading, data: targetListData } = useFetchTargetList();
+  const { isLoading: equipmentListIsLoading, data: equipmentListData } = useFetchEquipmentList();
 
-  if (isLoading || targetListIsLoading || !data || !targetListData) {
+  if (
+    isLoading ||
+    targetListIsLoading ||
+    equipmentListIsLoading ||
+    !data ||
+    !targetListData ||
+    !equipmentListData
+  ) {
     return <h1>Loading</h1>;
   }
 
@@ -41,6 +49,26 @@ const ExercisesPage = () => {
         </div>
       </div>
       {/* Exercises by Equipment */}
+      <div className='my-4 '>
+        <h2 className='mb-3 text-4xl font-semibold'>Exercises by Equipment</h2>
+        {/* <div className='flex flex-wrap gap-2'> */}
+        <div className='flex flex-wrap gap-2'>
+          {equipmentListData.map((equipment) => (
+            <Link
+              key={`target-${equipment}`}
+              // variant='outline'
+              href={`/exercises/equipments/${equipment}`}
+              className={cn(
+                buttonVariants({ variant: 'outline' }),
+                'capitalize',
+                'max-sm:h-10 max-sm:rounded-md max-sm:px-8',
+              )}
+            >
+              {equipment.replaceAll('-', ' ')}
+            </Link>
+          ))}
+        </div>
+      </div>
       <Separator className='my-4' />
       <ExercisesGroup exercises={data} />
     </section>
