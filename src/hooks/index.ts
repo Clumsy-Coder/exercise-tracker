3ceb95c9fb733eee89f2ace5a1090135fc5abe36
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-02-13 14:45:43 -0700

    chore(hooks): add react-query hook to fetch exercise `target list`
      ## what
      - add react-query hook to fetch exercise `target list`
      - add enum for fetching exercise `target list`
    
      ## how
      - fetching from endpoint `/api/exercises/targets`
      - use enum `QueryKey.targetList` as queryKey
    
      ## why
      - used for fetching exercise `target list`
    
      ## where
      - ./src/hooks/index.ts
    
      ## usage
    
      ## issue or pull request

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,12 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
 
-import { fetchAllExercises } from '@/server/actions';
+import { fetchAllExercises, fetchTargetList } from '@/server/actions';
 
 export enum QueryKey {
   /**
    * Query key for fetching all exercises
    */
   allExercises = 'all-exercises',
+
+  /**
+   * Query key for fetching target list
+   */
+  targetList = 'target-list',
 }
 
 // --------------------------------------------------------------------------------------------- //
@@ -20,6 +25,21 @@ export const useFetchAllExercises = () => {
   return useQuery({
     queryKey: [QueryKey.allExercises],
     queryFn: fetchAllExercises,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    staleTime: Infinity,
+  });
+};
+
+/**
+ * React query hook for fetching target list
+ *
+ * Uses server action `fetchTargetList`
+ */
+export const useFetchTargetList = () => {
+  return useQuery({
+    queryKey: [QueryKey.targetList],
+    queryFn: fetchTargetList,
     refetchOnMount: false,
     refetchOnWindowFocus: false,
     staleTime: Infinity,
