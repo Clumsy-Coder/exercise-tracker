66b61a55464371f33153fa70e19295ec4e8edd5e
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-02-13 14:50:25 -0700

    chore(layout:exercises): prefetch from `/api/exercises/targets`
      ## what
      - prefetch from `/api/exercises/targets`
    
      ## how
      - using react-query `QueryClient` to prefetch from api
    
      ## why
      - this will be used for prefetching data from api
    
      ## where
      - ./src/app/exercises/layout.tsx
    
      ## usage
    
      ## issue or pull request

--- src/app/exercises/layout.tsx
+++ src/app/exercises/layout.tsx
@@ -2,13 +2,17 @@ import { PropsWithChildren } from 'react';
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
 import { QueryKey } from '@/hooks';
-import { fetchAllExercises } from '@/server/actions';
+import { fetchAllExercises, fetchTargetList } from '@/server/actions';
 
 const ExercisesLayout = async ({ children }: PropsWithChildren) => {
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
     queryKey: [QueryKey.allExercises],
     queryFn: async () => fetchAllExercises(),
+  });
+  await queryClient.prefetchQuery({
+    queryKey: [QueryKey.targetList],
+    queryFn: async () => fetchTargetList(),
   });
 
   return (
