ddb7fd2012ea1d5f722be1d2ae8ce69bf4d44f8e
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-02-17 11:19:55 -0700

    feat(components): add `Footer` component
      ## what
      - add `Footer` component
        - display version
        - display build ID
        - display build time
    
      ## how
    
      ## why
      - to display basic info about the app
    
      ## where
      - ./src/components/footer.tsx
    
      ## usage
    
      ## issue or pull request

---
+++ src/components/footer.tsx
@@ -0,0 +1,24 @@
+import moment from 'moment';
+
+const Footer = () => {
+  const appData = [
+    ['Version: ', process.env.NEXT_PUBLIC_BUILD_VERSION],
+    ['Build ID: ', process.env.NEXT_PUBLIC_BUILD_ID],
+    ['Build time: ', moment.unix(process.env.NEXT_PUBLIC_BUILD_TIME).format('YYYY-MM-DD')],
+  ];
+  return (
+    <footer className='flex flex-col items-center justify-center gap-8 bg-black py-5 text-white dark:border-t md:h-16 md:flex-row '>
+      {appData.map(([label, value]) => (
+        <div
+          key={`footer-${label}`}
+          className='flex items-center justify-between gap-1'
+        >
+          <h1 className='font-bold'>{label}</h1>
+          <p>{value}</p>
+        </div>
+      ))}
+    </footer>
+  );
+};
+
+export default Footer;
