afdb95363594fc43ce7758e0a706a2ac02a2f05f
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-02-20 08:48:53 -0700

    refactor(api:exercises:targets): use local data to fetch target list
      ## what
      - use local data to fetch target list
    
      ## how
    
      ## why
      - using the api to fetch the data is costly
    
      ## where
      - ./src/app/api/exercises/targets/route.ts
    
      ## usage
    
      ## issue or pull request

--- src/app/api/exercises/targets/route.ts
+++ src/app/api/exercises/targets/route.ts
@@ -1,14 +1,15 @@
-import { exerciseOptions, fetchTargetListUrl } from '@/utils/fetchData';
+import data from '@/utils/data/target.json';
+// import { exerciseOptions, fetchTargetListUrl } from '@/utils/fetchData';
 
 /**
  * Fetch target list
  */
 export const GET = async () => {
-  const url = fetchTargetListUrl();
-  const response = await fetch(url, exerciseOptions);
-  const data: string[] = await response.json();
+  // const url = fetchTargetListUrl();
+  // const response = await fetch(url, exerciseOptions);
+  // const data: string[] = await response.json();
 
   // regex for selecting all or any spaces
   const regex = /\s+/gi;
-  return Response.json(data.map((item) => item.replaceAll(regex, '-')));
+  return Response.json((data as string[]).map((item) => item.replaceAll(regex, '-')));
 };
