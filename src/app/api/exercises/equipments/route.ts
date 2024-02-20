5ed95da6663556a85a9239cf8e26ea0221f6b939
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-02-20 08:46:19 -0700

    refactor(api:exercise:equipments): use local data to fetch equipment list
      ## what
      - use local data to fetch equipment list
    
      ## how
    
      ## why
      - using the api to fetch the data is costly
    
      ## where
      - ./src/app/api/exercises/equipments/route.ts
    
      ## usage
    
      ## issue or pull request

--- src/app/api/exercises/equipments/route.ts
+++ src/app/api/exercises/equipments/route.ts
@@ -1,14 +1,15 @@
-import { exerciseOptions, fetchEquipmentListUrl } from '@/utils/fetchData';
+import data from '@/utils/data/equipment.json';
+// import { exerciseOptions, fetchEquipmentListUrl } from '@/utils/fetchData';
 
 /**
  * Fetch exercise equipment list
  */
 export const GET = async () => {
-  const url = fetchEquipmentListUrl();
-  const response = await fetch(url, exerciseOptions);
-  const data: string[] = await response.json();
+  // const url = fetchEquipmentListUrl();
+  // const response = await fetch(url, exerciseOptions);
+  // const data: string[] = await response.json();
 
   // regex for selecting all or any spaces
   const regex = /\s+/gi;
-  return Response.json(data.map((item) => item.replaceAll(regex, '-')));
+  return Response.json((data as string[]).map((item) => item.replaceAll(regex, '-')));
 };
