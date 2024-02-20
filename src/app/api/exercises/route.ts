802028340285fb99f4a18ccecaf3c78417f8ddfc
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-02-20 08:21:48 -0700

    refactor(api:exercises): use local data for all exercises
      ## what
      - use local data for all exercises
    
      ## how
    
      ## why
      - using the api to fetch the data is costly
    
      ## where
      - ./src/app/api/exercises/route.ts
    
      ## usage
    
      ## issue or pull request

--- src/app/api/exercises/route.ts
+++ src/app/api/exercises/route.ts
@@ -1,13 +1,14 @@
 import { Exercise } from '@/types/raw';
-import { exerciseOptions, fetchAllExercisesUrl } from '@/utils/fetchData';
+// import { exerciseOptions, fetchAllExercisesUrl } from '@/utils/fetchData';
+import data from '@/utils/data/exercises.json';
 
 /**
  * Fetch all exercises
  */
 export const GET = async () => {
-  const url = fetchAllExercisesUrl(20000);
-  const response = await fetch(url, exerciseOptions);
-  const data: Exercise[] = await response.json();
+  // const url = fetchAllExercisesUrl(20000);
+  // const response = await fetch(url, exerciseOptions);
+  // const data: Exercise[] = await response.json();
 
-  return Response.json(data);
+  return Response.json(data as Exercise[]);
 };
