d7a844ed58bae843b4a33b81fd7fcae97c24bab7
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-02-21 08:04:52 -0700

    chore(schema): add schema for `/api/exercises/equipments/[equipment]`
      ## what
      - add schema for `/api/exercises/equipments/[equipment]`
    
      ## how
      - using zod to build a schema
    
      ## why
      - this will be used for validating the query params for
        - `/api/exercises/equipments/[equipment]`
        - `/exercises/equipments/[equipment]`
    
      ## where
      - ./src/schema/index.ts
    
      ## usage
    
      ## issue or pull request

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -10,3 +10,12 @@ export const exerciseIdSchema = z.object({
     .number({ invalid_type_error: 'Exercise ID must be a number' })
     .min(1, 'Exercise ID must be a number greater than 0'),
 });
+
+/**
+ * Schema validation for endpoint `/api/exercises/equipments/[equipment]`
+ *
+ * Also used in `./src/app/exercises/equipments/[equipment]/page.tsx`
+ */
+export const exerciseEquipment = z.object({
+  equipment: z.coerce.string().min(1, 'Exercise equipment must be at least 1 character long'),
+});
