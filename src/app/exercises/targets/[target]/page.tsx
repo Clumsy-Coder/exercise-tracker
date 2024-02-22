52d9cbf8803e3c9ae36529bc76054f83bb409d77
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-02-22 09:19:26 -0700

    feat(page:exercises:targets): add page for `/exercises/targets/[target]`
      ## what
      - add page for `/exercises/targets/[target]`
        - display
          - exercise target name
          - exercises
          - pagination
          - search box
        - display error 404 if exercise target doesn't exist
        - display error 400 if there's an error
    
      ## how
      - use react-hook to fetch data from `/api/exercises/targets/[target]`
    
      ## why
      - this will be used to display exercises by target
    
      ## where
      - ./src/app/exercises/targets/[target]/page.tsx
    
      ## usage
    
      ## issue or pull request
      - #68

---
+++ src/app/exercises/targets/[target]/page.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { AxiosError } from 'axios';
+import { z } from 'zod';
+
+import TargetExercisesPageLoading from '@/app/exercises/targets/[target]/loading';
+import Error from '@/components/error';
+import ExercisesGroup from '@/components/exercisesGroup';
+import { Separator } from '@/components/ui/separator';
+import { useFetchTargetExercises } from '@/hooks';
+import { exerciseTarget as schema } from '@/schema';
+
+type Props = {
+  params: z.infer<typeof schema>;
+};
+
+const ExercisesPage = ({ params }: Props) => {
+  const { isPending, data, isError, error } = useFetchTargetExercises(params.target);
+
+  if (isPending) {
+    return <TargetExercisesPageLoading />;
+  }
+
+  if (isError) {
+    type ErrorMessage = {
+      message: string;
+    };
+
+    const status = (error as AxiosError<ErrorMessage>).response?.status;
+    const message = (error as AxiosError<ErrorMessage>).response?.data.message;
+
+    return (
+      <Error
+        status={status as number}
+        message={message}
+      />
+    );
+  }
+
+  return (
+    <section className='flex flex-col gap-5'>
+      <div className='flex flex-wrap gap-5 whitespace-nowrap text-5xl capitalize'>
+        <p className='font-bold '>{params.target}</p>
+        <p className='font-thin'>Exercises</p>
+      </div>
+      <Separator className='my-4 hidden lg:flex ' />
+      <ExercisesGroup exercises={data} />
+    </section>
+  );
+};
+
+export default ExercisesPage;
