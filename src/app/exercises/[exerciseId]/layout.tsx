a9b715723b1c8c289064b8c4b96b57e8138e5b25
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-02-26 09:04:25 -0700

    chore(layout:exercises:exerciseId): add metadata for `/exercises/[exerciseId]` page
      ## what
      - add metadata for `/exercises/[exerciseId]` page
        - add title of the page
    
      ## how
      - check
        - https://nextjs.org/docs/app/building-your-application/optimizing/metadata
    
      ## why
      - the `title` will be displayed in browser tab
    
      ## where
      - ./src/app/exercises/[exerciseId]/layout.tsx
    
      ## usage
    
      ## issue or pull request

--- src/app/exercises/[exerciseId]/layout.tsx
+++ src/app/exercises/[exerciseId]/layout.tsx
@@ -1,14 +1,37 @@
 import { PropsWithChildren } from 'react';
+import type { Metadata } from 'next';
+import { z } from 'zod';
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
 import { QueryKey } from '@/hooks';
 import { fetchExercise } from '@/server/actions';
 import { exerciseIdSchema as schema } from '@/schema';
-import { z } from 'zod';
+import data from '@/utils/data/exercises.json';
 
 type Props = {
   params: z.infer<typeof schema>;
 } & PropsWithChildren;
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { exerciseId } = params;
+
+  const exercise = data.find((item) => item.id === `${exerciseId}`);
+
+  if (exercise) {
+    return {
+      title: `'${exercise.name
+        .split(' ')
+        .map((item) => {
+          return item.charAt(0).toUpperCase() + item.slice(1);
+        })
+        .join(' ')}' - Exercise-tracker`,
+    };
+  }
+
+  return {
+    title: `'${exerciseId}' Not found - Exercise-tracker`,
+  };
+}
 
 const ExerciseIdLayout = async ({ children, params }: Props) => {
   const queryClient = new QueryClient();
