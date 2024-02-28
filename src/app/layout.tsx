3905358c67f509ccd953aa1c117012c8b98799ef
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-02-27 19:41:52 -0700

    chore(layout:home): use `SessionProvider` from `next-auth`
      ## what
      - use `SessionProvider` from `next-auth`
    
      ## how
      - import the component from `./src/components/providers/userAuthSessionProvider.tsx`
    
      ## why
      - this will allow getting the session on a client component
    
      ## where
      - ./src/app/layout.tsx
    
      ## usage
    
      ## issue or pull request

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Inter as FontSans } from 'next/font/google';
+import { getServerSession } from 'next-auth';
 
 import { ModeToggle } from '@/components/darkmode-toggle';
 import Footer from '@/components/footer';
 import { ReactQueryClientProvider } from '@/components/reactQueryClientProvider';
 import { ThemeProvider } from '@/components/theme-provider';
 import Navbar from '@/components/navbar';
+import SessionProvider from '@/components/providers/userAuthSessionProvider';
 import { cn } from '@/lib/utils';
 import './globals.css';
 
@@ -26,7 +28,9 @@ export const metadata: Metadata = {
   },
 };
 
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
+const RootLayout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await getServerSession();
+
   return (
     <html
       lang='en'
@@ -34,35 +38,37 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
     >
       {/* obtained from https://ui.shadcn.com/docs/installation/next#fonts  */}
       <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
-        <ReactQueryClientProvider>
-          <ThemeProvider
-            attribute='class'
-            defaultTheme='dark'
-            enableSystem
-            disableTransitionOnChange
-          >
-            {/*
+        <SessionProvider session={session}>
+          <ReactQueryClientProvider>
+            <ThemeProvider
+              attribute='class'
+              defaultTheme='dark'
+              enableSystem
+              disableTransitionOnChange
+            >
+              {/*
                 obtained from
                 https://github.com/shadcn-ui/taxonomy/blob/main/app/(dashboard)/dashboard/layout.tsx
             */}
-            <div className='flex min-h-screen flex-col space-y-6'>
-              <header className='sticky top-0 z-40 border-b bg-background'>
-                <div className='container flex h-16 items-center justify-between py-4'>
-                  <Navbar />
-                  <ModeToggle />
-                </div>
-              </header>
-              {/* <div className='container grid flex-1 gap-12 md:grid-cols-[200px_1fr]'> */}
-              {/*   <aside className='hidden w-[200px] flex-col md:flex'> */}
-              {/*     <DashboardNav items={dashboardConfig.sidebarNav} /> */}
-              {/*   </aside> */}
-              <main className='flex w-full flex-1 flex-col overflow-hidden'>{children}</main>
-              {/* </div> */}
-            </div>
-            <Footer />
-            <Analytics />
-          </ThemeProvider>
-        </ReactQueryClientProvider>
+              <div className='flex min-h-screen flex-col space-y-6'>
+                <header className='sticky top-0 z-40 border-b bg-background'>
+                  <div className='container flex h-16 items-center justify-between py-4'>
+                    <Navbar />
+                    <ModeToggle />
+                  </div>
+                </header>
+                {/* <div className='container grid flex-1 gap-12 md:grid-cols-[200px_1fr]'> */}
+                {/*   <aside className='hidden w-[200px] flex-col md:flex'> */}
+                {/*     <DashboardNav items={dashboardConfig.sidebarNav} /> */}
+                {/*   </aside> */}
+                <main className='flex w-full flex-1 flex-col overflow-hidden'>{children}</main>
+                {/* </div> */}
+              </div>
+              <Footer />
+              <Analytics />
+            </ThemeProvider>
+          </ReactQueryClientProvider>
+        </SessionProvider>
       </body>
     </html>
   );
