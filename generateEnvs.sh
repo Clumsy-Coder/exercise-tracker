e961608a4eb74d6dfe8834a8c92824c20fbfd75d
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-03-07 10:45:06 -0700

    chore(env): add script to generate ENV for `next-auth`
      ## what
      - add script to generate ENV for `next-auth`
        - GOOGLE_CLIENT_ID
        - GOOGLE_CLIENT_SECRET
        - NEXTAUTH_SECRET
        - NEXTAUTH_URL
    
      ## how
    
      ## why
      - this will be used to generate envs for the nextjs app
    
      ## where
      - ./generateEnvs.sh
    
      ## usage
    
      ## issue or pull request

--- generateEnvs.sh
+++ generateEnvs.sh
@@ -20,5 +20,21 @@ fi
   echo "NEXT_PUBLIC_BUILD_VERSION=$BUILD_VERSION"
   echo "NEXT_PUBLIC_BUILD_ID=$(npm run -s generateBuildId)"
   echo "NEXT_PUBLIC_BUILD_TIME=$(date +%s)"                       # build date & time in unix time
+  # ----------------------------------------------------------------------------------------------#
+  # next auth ENVs
+  #
+  # Google OAuth2.0 credentials
+  # - https://next-auth.js.org/providers/google
+  # - https://support.google.com/cloud/answer/6158849?hl=en
+  #
+  # echo "GOOGLE_CLIENT_ID="
+  # echo "GOOGLE_CLIENT_SECRET="
+  #
+  # next auth secrets.
+  # - https://next-auth.js.org/deployment
+  #
+  # echo "NEXTAUTH_SECRET="  # for encrypting JWT. generate using command     openssl rand -base64 100
+  # echo "NEXTAUTH_URL="     # for handling url redirect after logging in
+  # ----------------------------------------------------------------------------------------------#
 } > .env
 
