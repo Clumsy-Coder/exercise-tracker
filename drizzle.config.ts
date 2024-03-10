46ccd92d6462033090f59fea8346f140619956f2
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-03-10 15:08:50 -0600

    build(database): add `drizzle` config file
      ## what
      - add `drizzle` config file
    
      ## how
      - check
        - https://orm.drizzle.team/kit-docs/overview#configuration
    
      ## why
      - this will be used by `drizzle-kit` to
        - generate migration scripts
        - pushing changes to the database
    
      ## where
      - ./drizzle.config.ts
    
      ## usage
    
      ## issue or pull request

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import type { Config } from 'drizzle-kit';
+
+export default {
+  schema: './src/db/schema.ts',
+  out: './drizzle/migrations',
+  driver: 'pg',
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL!,
+  },
+  verbose: true,
+  strict: true,
+} satisfies Config;
