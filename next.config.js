b0f371b93bd19bf451d7b85f19883090dea8f40c
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-07-16 17:38:55 -0600

    build(nextjs:config): add env for build version, build id and build time
      ## what
      - add env for build version, build id and build time
        - get the env from terminal or obtained from running npm script
          - NEXT_PUBLIC_BUILD_VERSION: obtain from either
            - terminal env `NEXT_PUBLIC_BUILD_VERSION`
            - run npm script `npm run -s generateEnv`
          - NEXT_PUBLIC_BUILD_ID
            - terminal env `NEXT_PUBLIC_BUILD_ID`
            - run npm script `npm run -s generateBuildId`
          - NEXT_PUBLIC_BUILD_TIME
            - terminal env `NEXT_PUBLIC_BUILD_TIME`
            - run bash command `date +%s`
    
      ## how
      - it will check the env value is set in the terminal before running
        the `pnpm run dev` or `pnpm run build` command. If the value is NOT
        set in the terminal then obtain it by running the npm script
      - setting the env in `env` property
      - check
        - https://nextjs.org/docs/app/api-reference/next-config-js/env
    
      ## why
      - this is to set the env in the config file instead of generating the
        values and placing them in `.env.local` file; removes the need for
        using `generateEnvs.sh` script
    
      ## where
      - ./next.config.js
    
      ## usage
      - run `pnpm run dev` or `pnpm run build`
    
      ## issue or pull request
      - #435

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { execSync } = require('child_process');
+
 // eslint-disable-next-line tsdoc/syntax
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -19,6 +22,29 @@ const nextConfig = {
         pathname: '/image/**',
       },
     ],
+  },
+  // add custom envs
+  // check
+  // - https://nextjs.org/docs/app/api-reference/next-config-js/env
+  // - https://stackoverflow.com/a/71668310/3053548
+  //
+  // NOTE: this will override the env set in `.env*` files
+  //
+  // using this method to define the following envs, is because I'd like to generate these ENVs,
+  // without running `generateEnvs.sh` before running the nextjs app. Maybe you won't even need to
+  // use the `generateEnvs.sh` script
+  //
+  // ENVs in question
+  // - NEXT_PUBLIC_BUILD_VERSION
+  // - NEXT_PUBLIC_BUILD_ID
+  // - NEXT_PUBLIC_BUILD_TIME
+  //
+  env: {
+    NEXT_PUBLIC_BUILD_VERSION:
+      process.env.NEXT_PUBLIC_BUILD_VERSION || execSync('npm run -s generateVersion').toString(),
+    NEXT_PUBLIC_BUILD_ID:
+      process.env.NEXT_PUBLIC_BUILD_ID || execSync('npm run -s generateBuildId').toString(),
+    NEXT_PUBLIC_BUILD_TIME: process.env.NEXT_PUBLIC_BUILD_TIME || execSync('date +%s').toString(),
   },
 };
 
