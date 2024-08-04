bbd5fb29bfe5e21fcb44ca7817e8c003ba60bece
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-08-04 11:31:30 -0600

    refactor(api:exercises): fetch the exercises data from a remote source
      ## what
      - fetch the exercises data from a remote source
    
      ## how
      - the exercises data will be hosted on github repo
      - nextjs will fetch the data based on the ENV
        `NEXT_PUBLIC_EXERCISE_DB_LOCAL_FETCH_BASE_URL` or using `DEFAULT_EXERCISE_DB_LOCAL_FETCH_BASE_URL`
    
      ## why
      - before the exercises data were included in nextjs build, which made
        the size big. Moving it to a remote source reduces the size of the
        build
    
      ## where
      - ./src/app/api/exercises/route.ts
    
      ## usage
    
      ## issue or pull request

--- src/app/api/exercises/route.ts
+++ src/app/api/exercises/route.ts
@@ -1,14 +1,17 @@
 import { Exercise } from '@/types/raw';
-// import { exerciseOptions, fetchAllExercisesUrl } from '@/utils/fetchData';
-import data from '@/utils/data/exercises.json';
+import { DEFAULT_EXERCISE_DB_LOCAL_FETCH_BASE_URL } from '@/utils/fetchData';
 
 /**
  * Fetch all exercises
  */
 export const GET = async () => {
-  // const url = fetchAllExercisesUrl(20000);
-  // const response = await fetch(url, exerciseOptions);
-  // const data: Exercise[] = await response.json();
+  const baseUrl =
+    process.env.NEXT_PUBLIC_EXERCISE_DB_LOCAL_FETCH_BASE_URL ||
+    DEFAULT_EXERCISE_DB_LOCAL_FETCH_BASE_URL;
+  const url = `${baseUrl}/data/exercises.json`;
+  // console.debug('all exercises url: ', url);
+  const response = await fetch(url);
+  const data: Exercise[] = await response.json();
 
   return Response.json(data as Exercise[]);
 };
