455755aac5f3286709f9f1afe4441fb4bb21f5df
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-08-04 12:13:49 -0600

    refactor(api:exercises:targets): fetch exercise target list data from remote source
      ## what
      - fetch exercise target list data from remote source
    
      ## how
      - the exercise target list will be hosted on github repo
      - nextjs will fetch the data based on the ENV
        `NEXT_PUBLIC_EXERCISE_DB_LOCAL_FETCH_BASE_URL` or using `DEFAULT_EXERCISE_DB_LOCAL_FETCH_BASE_URL`
    
      ## why
      - before the exercise target list data were included in nextjs build, which made
        the size big. Moving it to a remote source reduces the size of the
        build
    
      ## where
      - ./src/app/api/exercises/targets/route.ts
    
      ## usage
    
      ## issue or pull request

--- src/app/api/exercises/targets/route.ts
+++ src/app/api/exercises/targets/route.ts
@@ -1,15 +1,19 @@
-import data from '@/utils/data/target.json';
-// import { exerciseOptions, fetchTargetListUrl } from '@/utils/fetchData';
+import { DEFAULT_EXERCISE_DB_LOCAL_FETCH_BASE_URL } from '@/utils/fetchData';
 
 /**
  * Fetch target list
  */
 export const GET = async () => {
-  // const url = fetchTargetListUrl();
-  // const response = await fetch(url, exerciseOptions);
-  // const data: string[] = await response.json();
+  const baseUrl =
+    process.env.NEXT_PUBLIC_EXERCISE_DB_LOCAL_FETCH_BASE_URL ||
+    DEFAULT_EXERCISE_DB_LOCAL_FETCH_BASE_URL;
+  const url = `${baseUrl}/data/target.json`;
+  // console.debug('equipment url: ', url);
+  const response = await fetch(url);
+  const data: string[] = await response.json();
 
   // regex for selecting all or any spaces
+  // replace any space with -
   const regex = /\s+/gi;
   return Response.json((data as string[]).map((item) => item.replaceAll(regex, '-')));
 };
