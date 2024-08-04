fc6a5ee7d2e0aaaa81fd497185ec74e52af2bd42
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-08-04 11:45:30 -0600

    refactor(api:exercises:bodyParts): fetch exercise body part data from remote source
      ## what
      - fetch exercise body part data from remote source
    
      ## how
      - the exercise body list will be hosted on github repo
      - nextjs will fetch the data based on the ENV
        `NEXT_PUBLIC_EXERCISE_DB_LOCAL_FETCH_BASE_URL` or using `DEFAULT_EXERCISE_DB_LOCAL_FETCH_BASE_URL`
    
      ## why
      - before the exercise body parts data were included in nextjs build, which made
        the size big. Moving it to a remote source reduces the size of the
        build
    
      ## where
      - ./src/app/api/exercises/body-parts/route.ts
    
      ## usage
    
      ## issue or pull request

--- src/app/api/exercises/body-parts/route.ts
+++ src/app/api/exercises/body-parts/route.ts
@@ -1,10 +1,19 @@
-import data from '@/utils/data/bodyPart.json';
+import { DEFAULT_EXERCISE_DB_LOCAL_FETCH_BASE_URL } from '@/utils/fetchData';
 
 /**
  * Fetch exercise body part list
  */
 export const GET = async () => {
+  const baseUrl =
+    process.env.NEXT_PUBLIC_EXERCISE_DB_LOCAL_FETCH_BASE_URL ||
+    DEFAULT_EXERCISE_DB_LOCAL_FETCH_BASE_URL;
+  const url = `${baseUrl}/data/bodyPart.json`;
+  // console.debug('body part url: ', url);
+  const response = await fetch(url);
+  const data: string[] = await response.json();
+
   // regex for selecting all or any spaces
+  // replace any space with -
   const regex = /\s+/gi;
   return Response.json((data as string[]).map((item) => item.replaceAll(regex, '-')));
 };
