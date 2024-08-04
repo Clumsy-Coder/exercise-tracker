b54e25154c239a3d5e90a92b367ac6a2dd2a18c3
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-08-04 13:09:54 -0600

    refactor(api:exercises:targets:[target]): fetch exercises data from remote source
      ## what
      - fetch exercises data from remote source
    
      ## how
      - the exercise data will be hosted on github repo
      - nextjs will fetch the data based on the ENV
        `NEXT_PUBLIC_EXERCISE_DB_LOCAL_FETCH_BASE_URL` or using `DEFAULT_EXERCISE_DB_LOCAL_FETCH_BASE_URL`
    
      ## why
      - before the exercise data were included in nextjs build, which made
        the size big. Moving it to a remote source reduces the size of the
        build
    
      ## where
      - ./src/app/api/exercises/targets/[target]/route.ts
    
      ## usage
    
      ## issue or pull request

--- src/app/api/exercises/targets/[target]/route.ts
+++ src/app/api/exercises/targets/[target]/route.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 
 import { exerciseTarget as schema } from '@/schema';
 import { Exercise } from '@/types/raw';
-import allExercisesData from '@/utils/data/exercises.json';
+import { DEFAULT_EXERCISE_DB_LOCAL_FETCH_BASE_URL } from '@/utils/fetchData';
 
 type GetParamsType = {
   params: z.infer<typeof schema>;
@@ -28,14 +28,22 @@ export const GET = async (_request: Request, { params }: GetParamsType) => {
   }
 
   // --------------------------------------------------------------------------------------------//
+  // fetch the exercises data from remote source
+  const baseUrl =
+    process.env.NEXT_PUBLIC_EXERCISE_DB_LOCAL_FETCH_BASE_URL ||
+    DEFAULT_EXERCISE_DB_LOCAL_FETCH_BASE_URL;
+  const url = `${baseUrl}/data/exercises.json`;
+  // console.debug('target [target] url: ', url);
+  const response = await fetch(url);
+  const data: Exercise[] = await response.json();
+
+  // --------------------------------------------------------------------------------------------//
 
   // get query param
   const { target } = params;
 
   // filter exercises by target
-  const filtered = (allExercisesData as Exercise[]).filter(
-    (item) => item.target === `${target.replaceAll('-', ' ')}`,
-  );
+  const filtered = data.filter((item) => item.target === `${target.replaceAll('-', ' ')}`);
 
   if (!filtered.length) {
     const message = {
