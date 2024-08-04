c14539caee2ff58060923ef08828326d1ddb8751
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-08-04 11:56:18 -0600

    refactor(api:exercises:equipments): fetch exercise equipment list data from remote source
      ## what
      - fetch exercise equipment list data from remote source
    
      ## how
      - the exercise equipment list will be hosted on github repo
      - nextjs will fetch the data based on the ENV
        `NEXT_PUBLIC_EXERCISE_DB_LOCAL_FETCH_BASE_URL` or using `DEFAULT_EXERCISE_DB_LOCAL_FETCH_BASE_URL`
    
      ## why
      - before the exercise equipment list data were included in nextjs build, which made
        the size big. Moving it to a remote source reduces the size of the
        build
    
      ## where
      - ./src/app/api/exercises/equipments/route.ts
    
      ## usage
    
      ## issue or pull request

--- src/app/api/exercises/equipments/route.ts
+++ src/app/api/exercises/equipments/route.ts
@@ -1,15 +1,19 @@
-import data from '@/utils/data/equipment.json';
-// import { exerciseOptions, fetchEquipmentListUrl } from '@/utils/fetchData';
+import { DEFAULT_EXERCISE_DB_LOCAL_FETCH_BASE_URL } from '@/utils/fetchData';
 
 /**
  * Fetch exercise equipment list
  */
 export const GET = async () => {
-  // const url = fetchEquipmentListUrl();
-  // const response = await fetch(url, exerciseOptions);
-  // const data: string[] = await response.json();
+  const baseUrl =
+    process.env.NEXT_PUBLIC_EXERCISE_DB_LOCAL_FETCH_BASE_URL ||
+    DEFAULT_EXERCISE_DB_LOCAL_FETCH_BASE_URL;
+  const url = `${baseUrl}/data/equipment.json`;
+  // console.debug('equipment url: ', url);
+  const response = await fetch(url);
+  const data: string[] = await response.json();
 
   // regex for selecting all or any spaces
+  // replace any space with -
   const regex = /\s+/gi;
   return Response.json((data as string[]).map((item) => item.replaceAll(regex, '-')));
 };
