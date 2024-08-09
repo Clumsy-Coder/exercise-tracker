fc25757db93ed3b9311d0ae58681c28d55d58767
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-08-09 16:11:24 -0600

    refactor(layout:exercises:[exerciseId]): prefetch exerciseId data for prerendering
      ## what
      - prefetch exerciseId data for prerendering
        - fetch using the exerciseId url before setting the title (in the
          browser tab)
        - prefetch using the exerciseId url to prerender the page with the
          exercise data
       - fetch using exerciseId url
         - ex: https://raw.githubusercontent.com/Clumsy-Coder/exercise-tracker/484-split-exercises-data/.database/data/exerciseIdData/0001.json
    
      ## how
      - fetch using the exerciseId url
    
      ## why
      - this will be used to
        - check if the exercise id exists or not
        - prefetch the data that will allow the page to be prerendered
          - this will prevent fetching the data again since react-query is
            caching the data as well
    
      ## where
      - ./src/app/exercises/[exerciseId]/layout.tsx
    
      ## usage
    
      ## commit id, issue or pull request

--- src/app/exercises/[exerciseId]/layout.tsx
+++ src/app/exercises/[exerciseId]/layout.tsx
@@ -4,7 +4,6 @@ import { z } from 'zod';
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
 import { QueryKey } from '@/hooks';
-import { fetchExercise } from '@/server/actions';
 import { exerciseIdSchema as schema } from '@/schema';
 import { Exercise } from '@/types/raw';
 import { baseUrl } from '@/utils/fetchData';
@@ -14,38 +13,42 @@ type Props = {
 } & PropsWithChildren;
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const url = `${baseUrl()}/data/exercises.json`;
-  // console.debug('exercise [exerciseId] url: ', url);
-  const response = await fetch(url);
-  const data: Exercise[] = await response.json();
-
-  // --------------------------------------------------------------------------------------------//
-
   const { exerciseId } = params;
+  let layoutResponse: Metadata;
+  try {
+    const url = `${baseUrl()}/data/exerciseIdData/${exerciseId}.json`;
+    const response = await fetch(url);
+    const data: Exercise = await response.json();
 
-  const exercise = data.find((item) => item.id === `${exerciseId}`);
-
-  if (exercise) {
-    return {
-      title: `'${exercise.name
+    layoutResponse = {
+      title: `'${data.name
         .split(' ')
         .map((item) => {
           return item.charAt(0).toUpperCase() + item.slice(1);
         })
         .join(' ')}' - Exercise-tracker`,
     };
+  } catch (Error) {
+    layoutResponse = {
+      title: `'${exerciseId}' Not found - Exercise-tracker`,
+    };
   }
 
-  return {
-    title: `'${exerciseId}' Not found - Exercise-tracker`,
-  };
+  return layoutResponse;
 }
 
 const ExerciseIdLayout = async ({ children, params }: Props) => {
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
     queryKey: [QueryKey.exercise, params.exerciseId],
-    queryFn: async () => fetchExercise(params.exerciseId),
+    queryFn: async () => {
+      const url = `${baseUrl()}/data/exerciseIdData/${params.exerciseId}.json`;
+      // console.debug('target [target] url: ', url);
+      const response = await fetch(url);
+      const data: Exercise = await response.json();
+
+      return data;
+    },
   });
 
   return (
