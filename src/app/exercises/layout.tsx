eb8f8ff7cff33e7fad043ccdc1eca9d3bef8915a
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-08-12 12:10:04 -0600

    refactor(layout:exercises): prefetch `exercises`, `target`, `equipment` and `body part` list
      ## what
      - prefetch `exercises`, `target`, `equipment` and `body part` list
      - force cache on the data fetched
        - the prerender doesn't work without it
    
      ## how
    
      ## why
      - this will be used to
        - prefetch the data so the page can be prerendred
          - this will prevent fetching the data again since react-query is
            caching the data as well
    
      ## where
      - ./src/app/exercises/layout.tsx
    
      ## usage
    
      ## commit id, issue or pull request

--- src/app/exercises/layout.tsx
+++ src/app/exercises/layout.tsx
@@ -3,7 +3,8 @@ import type { Metadata } from 'next';
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
 import { QueryKey } from '@/hooks';
-import { fetchAllExercises, fetchEquipmentList, fetchTargetList } from '@/server/actions';
+import { Exercise } from '@/types/raw';
+import { baseUrl } from '@/utils/fetchData';
 
 export const metadata: Metadata = {
   title: 'All Exercises - Exercise-tracker',
@@ -11,18 +12,52 @@ export const metadata: Metadata = {
 };
 
 const ExercisesLayout = async ({ children }: PropsWithChildren) => {
+  const regex = /\s+/gi;
+
   const queryClient = new QueryClient();
+  // fetch all exercises
   await queryClient.prefetchQuery({
     queryKey: [QueryKey.allExercises],
-    queryFn: async () => fetchAllExercises(),
+    queryFn: async () => {
+      const url = `${baseUrl()}/data/exercises.json`;
+      const response = await fetch(url, { cache: 'force-cache' });
+      const data: Exercise[] = await response.json();
+
+      return data;
+    },
   });
+  // fetch target list
   await queryClient.prefetchQuery({
     queryKey: [QueryKey.targetList],
-    queryFn: async () => fetchTargetList(),
+    queryFn: async () => {
+      const url = `${baseUrl()}/data/target.json`;
+      const response = await fetch(url, { cache: 'force-cache' });
+      const data: string[] = await response.json();
+
+      return data.map((item) => item.replaceAll(regex, '-'));
+    },
   });
+  // fetch equipment list
   await queryClient.prefetchQuery({
     queryKey: [QueryKey.equipmentList],
-    queryFn: async () => fetchEquipmentList(),
+    queryFn: async () => {
+      const url = `${baseUrl()}/data/equipment.json`;
+      const response = await fetch(url, { cache: 'force-cache' });
+      const data: string[] = await response.json();
+
+      return data.map((item) => item.replaceAll(regex, '-'));
+    },
+  });
+  // fetch body part list
+  await queryClient.prefetchQuery({
+    queryKey: [QueryKey.bodyPartList],
+    queryFn: async () => {
+      const url = `${baseUrl()}/data/bodyPart.json`;
+      const response = await fetch(url, { cache: 'force-cache' });
+      const data: string[] = await response.json();
+
+      return data.map((item) => item.replaceAll(regex, '-'));
+    },
   });
 
   return (
