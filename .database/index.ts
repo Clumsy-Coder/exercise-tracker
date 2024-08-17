f3549209fc0e183fea3a767e8ef9f15ca3498bc9
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-08-17 12:35:48 -0600

    build(data): add option to split exercises by `bodyPart`
      ## what
      - add option to split exercises by `bodyPart`
      - add `commander` option to split exercises by bodyPart
    
      ## how
      - check if input file exists
        - exit program if it doesn't
      - check if folder `data/bodyPartExercises` exists
        - create directory if it doesn't
      - read input file
      - check if there's data in the input file
        - exit program if there's no data
      - group exercises by body part
        - use function `Object.groupBy`
      - loop through each body part exercises
        - write to file
    
      ## why
      - might be of use when needing to create exercises by body part
    
      ## where
      - ./.database/index.ts
    
      ## usage
    
      ## commit id, issue or pull request

--- .database/index.ts
+++ .database/index.ts
@@ -165,6 +165,54 @@ const processEquipmentExercises = (inputFile = EXERCISE_DATA_FILEPATH, targetDir
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+// const EXERCISE_EQUIPMENT_FILEPATH = "data/equipment.json"
+const EXERCISE_BY_BODY_PART_DIRPATH = "data/bodyPartExercises"
+
+/**
+ * Consolidate exercises by body part
+ */
+const processBodyPartExercises = (inputFile = EXERCISE_DATA_FILEPATH, targetDir = EXERCISE_BY_BODY_PART_DIRPATH) => {
+  const fullTargetDirPath = `${__dirname}/${targetDir}`;
+  if (!fs.existsSync(inputFile)) {
+    console.error(`File ${inputFile} doesn't exist`)
+    console.log('Exiting program')
+    process.exit(1)
+  }
+
+  // create directory ./exerciseIdData/ if it doesn't exist
+  if (!fs.existsSync(fullTargetDirPath)) {
+    console.log('creating directory ', fullTargetDirPath);
+    fs.mkdirSync(fullTargetDirPath, { recursive: true });
+  }
+
+  const jsonFilePath = path.join(__dirname, inputFile);
+  const exercises: Exercise[] = readJsonFile(jsonFilePath);
+  // const targets: string[] = readJsonFile(EXERCISE_TARGET_FILEPATH)
+
+  if (!exercises.length) {
+    console.error(`NO data to process for file ${inputFile}`)
+    console.log('Exiting program')
+
+    process.exit(1)
+  }
+
+  const groupedExercises = Object.groupBy(exercises, ({ bodyPart }) => bodyPart)
+
+  for (const [bodyPart, exercises] of Object.entries(groupedExercises)) {
+    // replace space with `-`
+    const filepath = `${targetDir}/${bodyPart.split(' ').join('-')}.json`
+    const data = JSON.stringify(exercises)
+
+    writeFile(filepath, data)
+      .then(() => console.log(`Successfully wrote ${filepath}`))
+      .catch((err: Error) => console.error(`Error writing to file ${filepath}:`, err))
+  }
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
 import { Command } from 'commander'
 
 const program = new Command();
@@ -203,6 +251,18 @@ program
     const { inputFile, targetDir } = options
 
     processEquipmentExercises(inputFile, targetDir)
+  })
+
+// add option for processing exercises by body part
+program
+  .command('processBodyPartExercises [options]')
+  .description('Read `exercises.json` and split them by exercise body part')
+  .option('-i --inputFile <filename.json>', "JSON file containing exercises", EXERCISE_DATA_FILEPATH)
+  .option('-d --targetDir <folder path>', "Directory to place the split json files", EXERCISE_BY_BODY_PART_DIRPATH)
+  .action((_, options) => {
+    const { inputFile, targetDir } = options
+
+    processBodyPartExercises(inputFile, targetDir)
   })
 
 program.parse(process.argv)
