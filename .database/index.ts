9b9262aec844b1e991c380fd393761436f4d6c45
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-08-17 17:28:45 -0600

    build(data): add function to split exercises by `target`, `equipment` and `bodyPart`
      ## what
      - add function to split exercises by `target`, `equipment` and `bodyPart`
    
      ## how
      - convert on the function (ex: processBodyPartExercises) into a
        generic type to handle grouping exercises by `target`, `equipment`,
        and `bodyPart`
    
      ## why
      - repeating code when grouping exercises, prone to errors
      - easier to maintain
    
      ## where
      - .database/index.ts
    
      ## usage
      ```ts
      processGroupedExercises(inputFile, targetDir, ({ bodyPart }) => bodyPart)
      ```
    
      ## commit id, issue or pull request

--- .database/index.ts
+++ .database/index.ts
@@ -213,6 +213,84 @@ const processBodyPartExercises = (inputFile = EXERCISE_DATA_FILEPATH, targetDir 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+/**
+ * Process exercises into a exercise group.
+ *
+ * This is used for grouping exercises by
+ * - `target`
+ * - `equipment`
+ * - `bodyPart`.
+ *
+ * Why this function over a dedicated function for exercise grouping?
+ * Repeating code. It can also be generalized into one function
+ *
+ * Check commit
+ * @see 3d50e49
+ * @see 7187647
+ * @see 67c14af
+ *
+ * @example
+ * group exercises by `target`
+ * ```ts
+ * processGroupedExercises(inputFile, targetDir, ({ target }) => target)
+ * ```
+ *
+ * @example
+ * group exercises by `equipment`
+ * ```ts
+ * processGroupedExercises(inputFile, targetDir, ({ equipment }) => equipment)
+ * ```
+ *
+ * @example
+ * group exercises by `bodyPart`
+ * ```ts
+ * processGroupedExercises(inputFile, targetDir, ({ bodyPart }) => bodyPart)
+ * ```
+ *
+ * @param inputFile - file containing an array of exercises
+ * @param targetDir - destination directory
+ * @param groupBy - how to split the exercises. A function that takes an Exercise and return the PropertyKey.  Ex: split by `target`, `equipment` or `bodyPart`
+ */
+const processGroupedExercises = (inputFile = EXERCISE_DATA_FILEPATH, targetDir: string, groupBy: (item: Pick<Exercise, 'target' | 'equipment' | 'bodyPart'>) => PropertyKey) => {
+  const fullTargetDirPath = `${__dirname}/${targetDir}`;
+  if (!fs.existsSync(inputFile)) {
+    console.error(`File ${inputFile} doesn't exist`)
+    console.log('Exiting program')
+    process.exit(1)
+  }
+
+  // create directory if it doesn't exist
+  if (!fs.existsSync(fullTargetDirPath)) {
+    console.log('creating directory ', fullTargetDirPath);
+    fs.mkdirSync(fullTargetDirPath, { recursive: true });
+  }
+
+  const jsonFilePath = path.join(__dirname, inputFile);
+  const exercises: Exercise[] = readJsonFile(jsonFilePath);
+  // const targets: string[] = readJsonFile(EXERCISE_TARGET_FILEPATH)
+
+  if (!exercises.length) {
+    console.error(`NO data to process for file ${inputFile}`)
+    console.log('Exiting program')
+
+    process.exit(1)
+  }
+
+  const groupedExercises = Object.groupBy(exercises, groupBy)
+
+  for (const [groupedExercise, exercises] of Object.entries(groupedExercises)) {
+    // replace space with `-`
+    const filepath = `${targetDir}/${groupedExercise.split(' ').join('-')}.json`
+    const data = JSON.stringify(exercises)
+
+    writeFile(filepath, data)
+      .then(() => console.log(`Successfully wrote ${filepath}`))
+      .catch((err: Error) => console.error(`Error writing to file ${filepath}:`, err))
+  }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
 import { Command } from 'commander'
 
 const program = new Command();
