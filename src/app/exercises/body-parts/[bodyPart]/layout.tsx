90e56d99bccec88ecb22020d31be92bcde2cdb8c
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-08-20 12:12:46 -0600

    refactor(layout:exercises:bodyParts:[bodyPart]): prefetch body part from url
      ## what
      - prefetch body part from url
    
      ## how
      - fetch the body part json from remote
      - if there's no body part found, return 404, (by throwing an error)
      - if found, return data
    
      ## why
      - no need to fetch the entire exercises json and filter by body part
      - just fetch the body part json and return that
      - less code, and easier to maintain
    
      ## where
      - ./src/app/exercises/body-parts/[bodyPart]/layout.tsx
    
      ## usage
    
      ## commit id, issue or pull request

--- src/app/exercises/body-parts/[bodyPart]/layout.tsx
+++ src/app/exercises/body-parts/[bodyPart]/layout.tsx
@@ -5,32 +5,53 @@ import { z } from 'zod';
 
 import { QueryKey } from '@/hooks';
 import { exerciseBodyPart as schema } from '@/schema';
-import { fetchBodyPartExercises } from '@/server/actions';
+import { baseUrl } from '@/utils/fetchData';
 
 type Props = {
   params: z.infer<typeof schema>;
 } & PropsWithChildren;
 
-export function generateMetadata({ params }: Props): Metadata {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { bodyPart } = params;
 
-  const capitalizedStr = bodyPart
-    .split('-')
-    .map((item) => {
-      return item.charAt(0).toUpperCase() + item.slice(1);
-    })
-    .join('-');
+  let layoutResponse: Metadata;
 
-  return {
-    title: `'${capitalizedStr}' body part exercises - Exercise-tracker`,
-  };
+  try {
+    const url = `${baseUrl()}/data/bodyPartExercises/${bodyPart.replaceAll(' ', '-')}.json`;
+    const response = await fetch(url);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const data = await response.json();
+
+    const capitalizedStr = bodyPart
+      .split('-')
+      .map((word) => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join('-');
+
+    layoutResponse = {
+      title: `'${capitalizedStr}' body part exercises - Exercise-tracker`,
+    };
+  } catch (error) {
+    layoutResponse = {
+      title: `Exercise body part '${bodyPart}' Not found - Exercise-tracker'`,
+    };
+  }
+
+  return layoutResponse;
 }
 
 const BodyPartExercisesLayout = async ({ children, params }: Props) => {
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
     queryKey: [QueryKey.exercise, QueryKey.bodyPart, params.bodyPart],
-    queryFn: async () => fetchBodyPartExercises(params.bodyPart),
+    queryFn: async () => {
+      const url = `${baseUrl()}/data/bodyPartExercises/${params.bodyPart.replaceAll(' ', '-')}.json`;
+      const response = await fetch(url);
+      const data = await response.json();
+
+      return data;
+    },
   });
 
   return (
