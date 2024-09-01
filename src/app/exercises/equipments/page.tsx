a76ffdda46fe7450d90cbd9efcd970a62c9d74d9
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-09-01 17:01:50 -0600

    fix(page:exercises): error when navigating to `/exercises/targets`, `/exercises/equipments` or `/exercises/body-parts`
      ## what
      - error when navigating to `/exercises/targets`, `/exercises/equipments` or `/exercises/body-parts`
    
      ## cause
      - when navigating to those urls, the layout file would prefetch from
        upstream. This issue was nextjs was thinking i'm loading an exercise
        id `targets`, `equipments` or `body-parts`, instead of loading the
        page `/exercises/targets/`. this was cause of the confusion
        - the prefetch url was being resolved as
          - `base_url/data/exerciseIdData/targets.json`
          - `base_url/data/exerciseIdData/equipments.json`
          - `base_url/data/exerciseIdData/body-parts.json`
    
      ## solution
      - add pages component for and redirect to `/exercises/` page
        - `/exercises/targets`
        - `/exercises/equipments`
        - `/exercises/body-parts`
    
      ## why
      - the error was caused when navigating to those urls mentioned above
    
      ## where
      - ./src/app/exercises/body-parts/page.tsx
      - ./src/app/exercises/equipments/page.tsx
      - ./src/app/exercises/targets/page.tsx
    
      ## usage
      - navigate to (expect to be redirected to `/exercises/`)
        - `/exercises/targets`
        - `/exercises/equipments`
        - `/exercises/body-parts`
    
      ## commit id, issue or pull request
      - issue #524
    
      ## notes
      - later will change url path for viewing a single exercise using
        exercise id to `/exercises/id/[exerciseId]`
      - also restructure the `.database` directory to be more clear of the
        filepath

---
+++ src/app/exercises/equipments/page.tsx
@@ -0,0 +1,11 @@
+import { redirect } from 'next/navigation';
+
+// type Props = {};
+
+const EquipmentsPage = () => {
+  redirect('/exercises');
+
+  // return <div>EquipmentsPage</div>;
+};
+
+export default EquipmentsPage;
