1a55edd8235f99734bab5c677bed152b585e12e4
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-09-22 13:20:48 -0600

    docs(page:exercises:bodyParts): add comment on why redirecting to `/exercises` page
      ## what
      - add comment on why redirecting to `/exercises` page
    
      ## how
    
      ## why
    
      ## where
      - ./src/app/exercises/body-parts/page.tsx
    
      ## usage
    
      ## commit id, issue or pull request
    
      ## notes

--- src/app/exercises/body-parts/page.tsx
+++ src/app/exercises/body-parts/page.tsx
@@ -3,6 +3,8 @@ import { redirect } from 'next/navigation';
 // type Props = {};
 
 const BodyPartsPage = () => {
+  // redirecting to `/exercises` page
+  // check issue https://github.com/Clumsy-Coder/exercise-tracker/issues/524
   redirect('/exercises');
 
   // return <div>BodyPartsPage</div>;
