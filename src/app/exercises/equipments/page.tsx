7d1d0eb4cfccccf84999136f0450e6f9322fa915
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-09-22 13:19:36 -0600

    docs(page:exercises:equipments): add comment on why redirecting to `/exercises` page
      ## what
      - add comment on why redirecting to `/exercises` page
    
      ## how
    
      ## why
    
      ## where
      - ./src/app/exercises/equipments/page.tsx
    
      ## usage
    
      ## commit id, issue or pull request
    
      ## notes

--- src/app/exercises/equipments/page.tsx
+++ src/app/exercises/equipments/page.tsx
@@ -3,6 +3,8 @@ import { redirect } from 'next/navigation';
 // type Props = {};
 
 const EquipmentsPage = () => {
+  // redirecting to `/exercises` page
+  // check issue https://github.com/Clumsy-Coder/exercise-tracker/issues/524
   redirect('/exercises');
 
   // return <div>EquipmentsPage</div>;
