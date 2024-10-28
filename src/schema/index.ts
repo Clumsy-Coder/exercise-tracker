ca7d926a052c9783d410a5e2a27687f95e1872ee
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-10-27 22:00:11 -0600

    chore(schema): add schema for `/api/exercises/id/[exerciseId]/activity`
      ## what
      - add schema for `/api/exercises/id/[exerciseId]/activity`
        - add schema
          - properties
            - exercise id
            - date
            - reps
            - weight
            - weight unit
            - distance
            - distance unit
            - duration
          - validation
            - if no data is provided
            - if one of `reps`, `weight` or `weight unit` is missing
            - if one of `distance` or `distance unit` is missing
    
      ## how
      - using zod
        - using property validation
        - using `superRefine` to validate grouped exercise entries
          - reps based
            - reps
            - weight
            - weight unit
          - distance based
            - distance
            - distance unit
    
      ## why
      - this will be used for validating POST request data received from the
        front end
        - will be used on page `/exercises/id/[exerciseId]`
    
      ## where
      - ./src/schema/index.ts
    
      ## usage
    
      ## commit id, issue or pull request
    
      ## notes
      - the code needs to be cleaned up to work with the server and client
        side
      - needs better documentation
      - add better error handling. ex: `duration`
      - add validation handling.

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,5 +1,8 @@
 import { z } from 'zod';
 
+import { distanceEnum, weightEnum } from '@/db/schema';
+import moment from 'moment';
+
 /**
  * Schema validation for endpoint `/api/exercises/[exerciseId]`
  *
@@ -37,3 +40,107 @@ export const exerciseTarget = z.object({
 export const exerciseBodyPart = z.object({
   bodyPart: z.coerce.string().min(1, `Exercise 'body part' must be at least 1 character long`),
 });
+
+// --------------------------------------------------------------------------------------------- //
+
+/**
+ * Schema validation for endpoint `/api/exercises/[exerciseId]/activity`
+ *
+ * Also used in `./src/app/exercises/[exerciseId]/page.tsx`
+ */
+export const exerciseEntry = z
+  .object({
+    exerciseId: z.coerce
+      .number({ invalid_type_error: 'Exercise ID must be a number' })
+      .min(1, 'Exercise ID must be a number greater than 0'),
+    date: z.coerce.date().default(() => new Date()),
+    //
+    // reps based
+    reps: z.coerce
+      .number({ invalid_type_error: 'Exercise reps must be a number' })
+      .min(0, 'Exercise reps must be at least 0')
+      .nullish(),
+    // .default(0),
+    weight: z.coerce.number().min(0).nullish(),
+    // .default(0),
+    weightUnit: z
+      .enum(weightEnum.enumValues, {
+        // custom error message for enum
+        // check:
+        // - https://github.com/colinhacks/zod/issues/580#issuecomment-1425044684
+        // - https://zod.dev/ERROR_HANDLING?id=customizing-errors-with-zoderrormap
+        errorMap: () => ({ message: "Weight unit can be either 'lbs' or 'kg'" }),
+      })
+      .nullish(),
+    // .optional()
+    // .default('lbs'),
+    //
+    // distance based
+    distance: z.coerce.number().min(0, 'Distance must be greater than 0').nullish(),
+    // .default(0),
+    distanceUnit: z.enum(distanceEnum.enumValues).optional(),
+    // .default('km'),
+    //
+    // duration based
+    // duration: z.coerce.date({
+    //   errorMap: () => ({
+    //     message: 'testting',
+    //   }),
+    // }),
+    duration: z
+      .string()
+      .time({
+        message: 'Exercise duration is required. In format HH:MM:SS',
+      })
+      .refine((duration) => moment(duration, 'HH:MM:SS', true).isValid(), {
+        message: 'Invalid time format. Must be HH:MM:SS',
+        path: ['duration'],
+      })
+      .nullish(),
+    // .optional(),
+    // .default(() => new Date('00:00:00')),
+  })
+  .superRefine((data, ctx) => {
+    const { reps, weight, weightUnit, distance, distanceUnit, duration } = data;
+    // if no data is provided
+    if (
+      [reps, weight, weightUnit, distance, distanceUnit, duration].every((v) => v === undefined)
+    ) {
+      ['reps', 'weight', 'weightUnit', 'distance', 'distanceUnit', 'duration'].forEach((cur) => {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message:
+            "Either provide 'reps', 'weight' and 'weight unit, OR provide 'distance' and 'distance unit' OR 'duration'",
+          path: [cur],
+        });
+      });
+    }
+
+    // if either `reps`, `weight` or `weight unit` is provided
+    if ([reps, weight, weightUnit].some((v) => v !== undefined)) {
+      // apply error message to missing properties
+      ['reps', 'weight', 'weightUnit']
+        .filter((cur) => data[cur as keyof typeof data] === undefined)
+        .forEach((cur) => {
+          ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: 'Required',
+            path: [cur],
+          });
+        });
+    }
+
+    // if either `distance`, or `distance unit` is provided
+    if ([distance, distanceUnit].some((v) => v !== undefined)) {
+      // apply error message to missing properties
+      ['distance', 'distanceUnit']
+        .filter((cur) => data[cur as keyof typeof data] === undefined)
+        .forEach((cur) => {
+          ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: "Provide 'Distance' and 'Distance unit'",
+            path: [cur],
+          });
+        });
+    }
+  });
