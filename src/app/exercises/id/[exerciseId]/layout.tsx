1c07196d9dd4c769d742ccf954156d0c6777044e
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-11-14 08:16:34 -0700

    feat(layout:exercises:[exerciseId]): prefetch exercise activity IF the user's logged in
      ## what
      - prefetch exercise activity IF the user's logged in
    
      ## how
      - check if the user's logged in
      - fetch the data using a prepared statement
        - check 81aca7f
    
      ## why
      - to prefetch the data for SSR
    
      ## where
      - ./src/app/exercises/id/[exerciseId]/layout.tsx
    
      ## usage
    
      ## commit id, issue or pull request
    
      ## notes

--- src/app/exercises/id/[exerciseId]/layout.tsx
+++ src/app/exercises/id/[exerciseId]/layout.tsx
@@ -1,8 +1,11 @@
 import { PropsWithChildren } from 'react';
 import type { Metadata } from 'next';
+import { getServerSession } from 'next-auth';
 import { z } from 'zod';
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { selectExerciseIdActivity } from '@/db/prepared-statements';
 import { QueryKey } from '@/hooks';
 import { exerciseIdSchema as schema } from '@/schema';
 import { Exercise } from '@/types/raw';
@@ -50,6 +53,25 @@ const ExerciseIdLayout = async ({ children, params }: Props) => {
       return data;
     },
   });
+
+  // prefetch exercise activity data IF the user's logged in
+  const session = await getServerSession(authOptions);
+  if (session) {
+    await queryClient.prefetchQuery({
+      queryKey: [QueryKey.exerciseIdActivity, params.exerciseId],
+      queryFn: async () => {
+        const dbData = await selectExerciseIdActivity.execute({
+          userId: session?.user?.email as string,
+          exerciseId: +params.exerciseId,
+        });
+
+        // putting in an object because the hook `useFetchExerciseIdActivity`
+        // returns as AxiosResponse; the data returned is located in property `data`
+        // if it's not done this way, nextjs will throw an error (in src/app/exercises/id/[exerciseId]/page.tsx)
+        return { data: dbData };
+      },
+    });
+  }
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
