9aa111b46744b8083012e321cdcaba06f99c9bf9
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-11-21 17:13:55 -0700

    feat(components:exerciseIdActivityTable:columns): use sorting component in column header
      ## what
      - use the sorting table column component when defining column headers
    
      ## how
      - use component `./src/components/exercise-activity-table/column-header.tsx`
    
      ## why
      - this allows the user to sort a column by `ascending`, `descending` or
        `default`
      - this allows the user to hide a column
    
      ## where
      - ./src/components/exercise-activity-table/columns.tsx
    
      ## usage
    
      ## commit id, issue or pull request
      - 103342c
    
      ## notes

--- src/components/exercise-activity-table/columns.tsx
+++ src/components/exercise-activity-table/columns.tsx
@@ -2,11 +2,21 @@ import { format } from 'date-fns';
 
 import { ExerciseActivity } from '@/types/raw';
 import { ColumnDef } from '@tanstack/react-table';
+import { DataTableColumnHeader } from './column-header';
+
+// https://tanstack.com/table/latest/docs/api/core/column-def#columndef-apis
 
 export const columns: ColumnDef<ExerciseActivity>[] = [
   {
     accessorKey: 'exerciseId',
-    header: 'Exercise ID',
+    header: ({ column }) => {
+      return (
+        <DataTableColumnHeader
+          column={column}
+          title='Exercise ID'
+        />
+      );
+    },
     accessorFn: (row) => `${row.exerciseId.padStart(4, '0').slice(-4)}`,
     meta: {
       headerTitle: 'Exercise ID',
@@ -14,17 +24,38 @@ export const columns: ColumnDef<ExerciseActivity>[] = [
   },
   {
     accessorKey: 'reps',
-    header: 'Reps',
+    header: ({ column }) => {
+      return (
+        <DataTableColumnHeader
+          column={column}
+          title='Reps'
+        />
+      );
+    },
     accessorFn: (row) => `${row.reps ?? '-'}`,
   },
   {
     accessorKey: 'weight',
-    header: 'Weight',
+    header: ({ column }) => {
+      return (
+        <DataTableColumnHeader
+          column={column}
+          title='Weight'
+        />
+      );
+    },
     accessorFn: (row) => `${row.weight ?? '-'}`,
   },
   {
     accessorKey: 'weightUnit',
-    header: 'Weight Unit',
+    header: ({ column }) => {
+      return (
+        <DataTableColumnHeader
+          column={column}
+          title='Weight Unit'
+        />
+      );
+    },
     accessorFn: (row) => `${row.weightUnit ?? '-'}`,
     meta: {
       headerTitle: 'Weight Unit',
@@ -32,12 +63,26 @@ export const columns: ColumnDef<ExerciseActivity>[] = [
   },
   {
     accessorKey: 'distance',
-    header: 'Distance',
+    header: ({ column }) => {
+      return (
+        <DataTableColumnHeader
+          column={column}
+          title='Distance'
+        />
+      );
+    },
     accessorFn: (row) => `${row.distance ?? '-'}`,
   },
   {
     accessorKey: 'distanceUnit',
-    header: 'Distance Unit',
+    header: ({ column }) => {
+      return (
+        <DataTableColumnHeader
+          column={column}
+          title='Distance Unit'
+        />
+      );
+    },
     accessorFn: (row) => `${row.distanceUnit ?? '-'}`,
     meta: {
       headerTitle: 'Distance Unit',
@@ -45,18 +90,41 @@ export const columns: ColumnDef<ExerciseActivity>[] = [
   },
   {
     accessorKey: 'duration',
-    header: 'Duration (HH:MM:SS)',
+    header: ({ column }) => {
+      return (
+        <DataTableColumnHeader
+          column={column}
+          title='Duration (HH:MM:SS)'
+        />
+      );
+    },
     accessorFn: (row) => `${row.duration ?? '-'}`,
   },
   {
     accessorKey: 'date',
-    header: 'Date',
-    accessorFn: (row) => `${format(new Date(row.date), 'ccc, PPpp')}`,
+    header: ({ column }) => {
+      return (
+        <DataTableColumnHeader
+          column={column}
+          title='Date'
+        />
+      );
+    },
+    // accessorFn: (row) => `${format(new Date(row.date), 'ccc, PPpp')}`,
+    cell: ({ row }) => `${format(new Date(row.getValue('date')), 'ccc, PPpp')}`,
     // header: () => <div className='mr-10 px-0 text-right'>Date</div>,
     // cell: ({ row }) => {
     //   const date = new Date(row.getValue('date'));
     //   const formatted = format(date, 'ccc, PPpp');
     //   return <div className='mr-10 text-right'>{formatted}</div>;
     // },
+    //
+    sortingFn: 'datetime',
+    // sort by date
+    // obtained from
+    // - https://stackoverflow.com/a/18246278/3053548
+    // - https://tanstack.com/table/v8/docs/guide/sorting#sorting-fns
+    // sortingFn: (rowA, rowB) =>
+    //   new Date(rowA.original.date).getTime() - new Date(rowB.original.date).getTime(),
   },
 ];
