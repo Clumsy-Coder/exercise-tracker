57ceb3cdddf74edf7cc46c21ed85d6c46cc86e3a
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-12-12 11:45:20 -0700

    build(database:prepared-statements): add prepared statement for checking if activity id exists
      ## what
      - select a row from table `activities`
        - narrow down by `id`
        - narrow down by user email
    
      ## how
      - https://orm.drizzle.team/docs/guides/count-rows
    
      ## why
      - used for checking if the activity id exists
      - used for checking before deleting activity id
    
      ## where
      - ./src/db/prepared-statements.ts
    
      ## usage
      ```ts
      import { checkActivityIdExists } from '@/db/prepared-statements';
    
      const dbActivityIdExistsData = await checkActivityIdExists.execute({
        userId: session?.user?.email as string,
        id: +formData.id,
      });
    
      if (dbActivityIdExistsData[0].count === 0) {
        // activity id doesn't exist
      }
      ```
    
      ## commit id, issue or pull request
    
      ## notes

--- src/db/prepared-statements.ts
+++ src/db/prepared-statements.ts
@@ -1,4 +1,4 @@
-import { eq, and, sql } from 'drizzle-orm';
+import { eq, and, sql, count } from 'drizzle-orm';
 
 import { db } from '@/db';
 import { activities } from '@/db/schema';
@@ -38,3 +38,29 @@ export const selectExerciseIdActivity = db
   )
   .orderBy(activities.date)
   .prepare('select_exerciseId_activity');
+
+/**
+ * Prepared statement for checking if activity exists using user email and activity `id`
+ *
+ * @see {@link https://orm.drizzle.team/docs/guides/count-rows}
+ * @example
+ * ```ts
+ * import { checkActivityIdExists } from '@/db/prepared-statements';
+ *
+ * const dbActivityIdExistsData = await checkActivityIdExists.execute({
+ *   userId: session?.user?.email as string,
+ *   id: +formData.id,
+ * });
+ *
+ * if (dbActivityIdExistsData[0].count === 0) {
+ *   // activity id doesn't exist
+ * }
+ * ```
+ */
+export const checkActivityIdExists = db
+  .select({ count: count() })
+  .from(activities)
+  .where(
+    and(eq(activities.id, sql.placeholder('id')), eq(activities.userId, sql.placeholder('userId'))),
+  )
+  .limit(1);
